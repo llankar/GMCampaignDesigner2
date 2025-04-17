aaef2858f96473b973b744a24f160759e2774f7a
llankar <lankar.laurent@gmail.com>
2025-04-17 23:35:18 +0200

    location look and feel modification

--- start_webserver.sh
+++ start_webserver.sh
@@ -7,4 +7,4 @@ cd /var/services/homes/llankar/Drive/rpg/Python/GMCampaignDesigner2
 
 # Run the Flask app using the module command.
 # (Make sure you run from the project root so Python locates your packages.)
-python -m modules.web.npc_graph_webviewer
+python -m modules.web.GM_webviewer
