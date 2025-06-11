0d2cd6753c6e1ed4dfc55c0e09bf1c038888a004
llankar <121680998+llankar@users.noreply.github.com>
2025-06-11 20:28:13 +0200

    fix map web display persistence

--- modules/maps/controllers/display_map_controller.py
+++ modules/maps/controllers/display_map_controller.py
@@ -11,7 +11,7 @@ from modules.maps.services.fog_manager import _set_fog, clear_fog, reset_fog, on
 # from modules.maps.services.token_manager import add_token, _on_token_press, _on_token_move, _on_token_release, _copy_token, _paste_token, _show_token_menu, _resize_token_dialog, _change_token_border_color, _delete_token, _persist_tokens
 from modules.maps.services.token_manager import add_token, _persist_tokens, _change_token_border_color # Keep this if it's used by other token_manager functions not moved
 from modules.maps.views.fullscreen_view import open_fullscreen, _update_fullscreen_map
-from modules.maps.views.web_display_view import open_web_display, _update_web_display_map
+from modules.maps.views.web_display_view import open_web_display, _update_web_display_map, close_web_display
 from modules.maps.services.entity_picker_service import open_entity_picker, on_entity_selected
 from modules.maps.utils.icon_loader import load_icon
 from PIL import Image, ImageTk, ImageDraw
@@ -928,6 +928,7 @@ class DisplayMapController:
     open_entity_picker = open_entity_picker
     open_fullscreen = open_fullscreen
     open_web_display = open_web_display
+    close_web_display = close_web_display
     reset_fog = reset_fog
     select_map = select_map
 
