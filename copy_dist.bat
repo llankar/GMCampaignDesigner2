a81f3531c09edbabb1b9a0287be47f0e85a7ec93
llankar <llankar@free.fr>
2025-08-16 14:34:33 +0200

    corrected size of gmscreen longtext display box. added bat to copy files to final exe

---
+++ copy_dist.bat
@@ -0,0 +1,3 @@
+xcopy /Y /S /R dist\main_window\* D:\SynologyDrive\CampaignManager
+xcopy /Y /S /R modules D:\SynologyDrive\CampaignManager
+xcopy /Y /S /R modules D:\SynologyDrive\CampaignManager\_internal
